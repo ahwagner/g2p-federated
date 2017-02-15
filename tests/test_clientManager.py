db912d4097da45c2b14cce4f8f852cbc1e720750
Alex H. Wagner, PhD <awagner24@wustl.edu>
2017-02-15 12:35:28 -0600

    add test framework

---
+++ tests/test_clientManager.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class TestClientManager(TestCase):
+    def test_add_http_client(self):
+        self.fail()
+
+    def test_add_local_client(self):
+        self.fail()
+
+    def test_restrictClient(self):
+        self.fail()
+
+    def test_load_clients_from_config(self):
+        self.fail()
+
+    def test_federated_featurephenotypeassociaton_query(self):
+        self.fail()
