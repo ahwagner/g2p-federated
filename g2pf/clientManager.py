660c5370b4d6507256c9c8352bc3c8c7776414e1
ozanozisik <ozanytu@gmail.com>
2017-03-24 00:36:24 +0300

    federated_featurephenotypeassociaton_query and related test

--- g2pf/clientManager.py
+++ g2pf/clientManager.py
@@ -37,9 +37,43 @@ class ClientManager:
         """Add g2p clients from a specified configparser-compliant config file."""
         pass
         
-    def federated_featurephenotypeassociaton_query(self, args):
+    def federated_featurephenotypeassociaton_query(self, feature_kwargs={},phenotype_kwargs={}):
         """Search all clients for feature-phenotype associations as defined by args."""
-        pass
+        if len(self.client_list)==0:
+            raise RuntimeError('No client is added to the manager')
+        federated_associations=[]
+        for c in self.client_list:
+            datasets = c.search_datasets()
+            datasets=[dataset for dataset in datasets if dataset.name not in c.restricted_datasets]
+            phenotype_association_set_ids = []
+            for  dataset in datasets:
+                #print dataset.name
+                feature_ids = []
+                phenotype_ids =[]
+                phenotype_association_sets = c.search_phenotype_association_sets(dataset_id=dataset.id)
+                for phenotype_association_set in phenotype_association_sets:
+                    phenotype_association_set_ids.append(phenotype_association_set.id)
+                    #print 'phenotype_association_set:', phenotype_association_set.id, phenotype_association_set.name
+                    if len(phenotype_kwargs)!=0:
+                        phenotypes_generator = c.search_phenotype(phenotype_association_set_id=phenotype_association_set.id,**phenotype_kwargs)
+                        for phenotype in phenotypes_generator:
+                            phenotype_ids.append(phenotype.id)
+                            #print "Phenotype",phenotype.id
+                featuresets = c.search_feature_sets(dataset_id=dataset.id)
+                for featureset in featuresets:
+                    #print 'feature_set:',featureset.id,featureset.name
+                    if len(feature_kwargs)!=0:
+                        feature_generator = c.search_features(feature_set_id=featureset.id,**feature_kwargs)
+                        for feature in feature_generator:
+                            feature_ids.append(feature.id)
+                            #print "Feature",[feature.name,feature.gene_symbol,feature.reference_name,feature.start,feature.end, feature.id]
+                for phenotype_association_set_id in phenotype_association_set_ids:  
+                    feature_phenotype_associations = c.search_genotype_phenotype(phenotype_association_set_id = phenotype_association_set_id,
+                                                                                 phenotype_ids=phenotype_ids, feature_ids=feature_ids)
+                    for association in feature_phenotype_associations:
+                        federated_associations.append(association)
+                        #print association.description
+        return federated_associations
 
 
 class HttpClient(client.HttpClient):
